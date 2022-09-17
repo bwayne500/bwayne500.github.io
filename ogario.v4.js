f91fcd1af6a64d19bb838593e95fc1d248350d45
bwayne500 <bwayne500@gmail.com>
2022-09-17 17:52:15 +0800

    Update ogario.v4.js

--- ogario.v4.js
+++ ogario.v4.js
@@ -1,5 +1,5 @@
 /* Source script
-v3.123
+v3.124
 Decoded simplified and modified by MGx, Adam, Jimboy3100, Snez, Volum, Alexander Lulko, Sonia, Yahnych, Davi SH
 This is part of the Legend mod project
 IF YOU A NORMAL PERSON AND CARE ABOUT YOUR HEALTH, DON'T READ THIS SCRIPT
